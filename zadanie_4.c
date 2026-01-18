d2a7c01106819674d59a8c89a647157bf6bebbbf
Adam Michałek <a.michalek.954@studms.ug.edu.pl>
2026-01-18 13:48:34 +0100

    Update zadanie_4.c
    Dodanie prototypu funkcji

--- zadanie_4.c
+++ zadanie_4.c
@@ -1,40 +1,6 @@
 #include <stdio.h>
 
-void grid_update(const int src[5][5], int dest[5][5]) {
-    // Iteracja po każdym polu w macierzy
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            // Oblicz sumę wartości bezpośrednich sąsiadów
-            int suma_sasiadow = 0;
-
-            // Sprawdź sąsiada z góry
-            if (i - 1 >= 0) {
-                suma_sasiadow += src[i - 1][j];
-            }
-            // Sprawdź sąsiada z dołu
-            if (i + 1 < 5) {
-                suma_sasiadow += src[i + 1][j];
-            }
-            // Sprawdź sąsiada z lewej
-            if (j - 1 >= 0) {
-                suma_sasiadow += src[i][j - 1];
-            }
-            // Sprawdź sąsiada z prawej
-            if (j + 1 < 5) {
-                suma_sasiadow += src[i][j + 1];
-            }
-
-            // Porównaj wartość src[i][j] z obliczoną sumą sąsiadów
-            if (src[i][j] > suma_sasiadow) {
-                dest[i][j] = 2; // Jeśli wartość większa od sumy, wpisz 2
-            } else if (src[i][j] == suma_sasiadow) {
-                dest[i][j] = 1; // Jeśli wartość równa sumie, wpisz 1
-            } else {
-                dest[i][j] = 0; // Jeśli wartość mniejsza od sumy, wpisz 0
-            }
-        }
-    }
-}
+void grid_update(const int src[5][5], int dest[5][5]);
 
 int main() {
     int src[5][5];
@@ -71,4 +37,40 @@ int main() {
     }
 
     return 0;
+}
+
+void grid_update(const int src[5][5], int dest[5][5]) {
+    // Iteracja po każdym polu w macierzy
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            // Oblicz sumę wartości bezpośrednich sąsiadów
+            int suma_sasiadow = 0;
+
+            // Sprawdź sąsiada z góry
+            if (i - 1 >= 0) {
+                suma_sasiadow += src[i - 1][j];
+            }
+            // Sprawdź sąsiada z dołu
+            if (i + 1 < 5) {
+                suma_sasiadow += src[i + 1][j];
+            }
+            // Sprawdź sąsiada z lewej
+            if (j - 1 >= 0) {
+                suma_sasiadow += src[i][j - 1];
+            }
+            // Sprawdź sąsiada z prawej
+            if (j + 1 < 5) {
+                suma_sasiadow += src[i][j + 1];
+            }
+
+            // Porównaj wartość src[i][j] z obliczoną sumą sąsiadów
+            if (src[i][j] > suma_sasiadow) {
+                dest[i][j] = 2; // Jeśli wartość większa od sumy, wpisz 2
+            } else if (src[i][j] == suma_sasiadow) {
+                dest[i][j] = 1; // Jeśli wartość równa sumie, wpisz 1
+            } else {
+                dest[i][j] = 0; // Jeśli wartość mniejsza od sumy, wpisz 0
+            }
+        }
+    }
 }

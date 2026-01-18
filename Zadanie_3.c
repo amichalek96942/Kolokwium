740f5fc1ff6c35b66333adff2cdb8d4c90946400
Adam Michałek <a.michalek.954@studms.ug.edu.pl>
2026-01-18 13:34:00 +0100

    Zadanie_3
    Zadanie_3

---
+++ Zadanie_3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int text_processor(const char *str);
+
+// Funkcja główna do testów
+int main() {
+    const char *test_str = "AHJ3";
+    int result = text_processor(test_str);
+    printf("Wynik dla testowego lancucha \"%s\": %d\n", test_str, result);
+
+    return 0;
+}
+
+int text_processor(const char *str) {
+    int total = 0;
+    int sign = 1;
+
+    // Przetwarzanie łańcucha znaków znak po znaku
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = str[i]; // Aktualny znak
+
+
+        if (ch >= 'A' && ch <= 'Z') {
+            total += 10 * sign;
+        }
+
+        else if (ch >= '0' && ch <= '9') {
+            int digit = ch - '0'; // Zamiana znaku na wartość cyfry
+            if (digit % 2 == 0) {
+                sign = 1; // Cyfra parzysta: ustaw znak na dodatni
+            } else {
+                sign = -1; // Cyfra nieparzysta: ustaw znak na ujemny
+            }
+        }
+        // Jeśli znak jest wykrzyknikiem ('!'), zresetuj total do zera
+        else if (ch == '!') {
+            total = 0;
+        }
+    }
+
+    return total; // Zwrócenie wyniku (wartości total)
+}
